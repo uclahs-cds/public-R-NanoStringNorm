e94ed5803fdb866cb4888a6ed1a7cafc47eefa7e
Dan Knight <danknight@mednet.ucla.edu>
2025-01-10 16:27:07 -0800

    Replace gdata Excel dependency with readxl

--- R/read.xls.RCC.R
+++ R/read.xls.RCC.R
@@ -93,47 +93,29 @@ read.xls.RCC <- function(xls, sheet = 1, perl, sample.id.row = "File.Name") {
 	sample.ids <- gsub("^([0-9])", "X\\1", sample.ids);
 	colnames(rcc$header) <- sample.ids;
 
-	# define pattern of first line of count data
-	pattern.first.line.counts <- "Code";
-
-	# call gdata::read.excel and load counts
-	x <- gdata::read.xls(
-		xls = xls,
-		sheet = sheet,
-		pattern = pattern.first.line.counts,
-		method = "tab",
-		perl = perl,
-		header = TRUE,
-		strip.white = TRUE,
-		as.is = TRUE
-		);
-
-	if (is.null(x)) {
+	if (is.null(rcc$x)) {
 		stop("READ.XLS.RCC: There appears to be a problem with RCC file. Likely couldnt find the count rcc$header specifically `Code Class`");
 		}
 
-	# drop any trailing columns 
-	x <- x[,1:(3+length(sample.ids))];
+    colnames(rcc$x) <- rcc$x[2, ];
+    rcc$x <- rcc$x[-c(1:2), 1:(3 + length(sample.ids))];
 
 	# drop rows that have a missing code class or gene name
-	rows.with.missing.anno <- (x[,1] == '' | x[,2] == '');
+	rows.with.missing.anno <- (rcc$x[, 1] == '' | rcc$x[, 2] == '');
 	if (any(rows.with.missing.anno)) {
+	    rcc$x <- rcc$x[!rows.with.missing.anno,];
 		cat(paste("The following row(s)", paste(which(rows.with.missing.anno), collapse = ", "), "have been dropped due to missing annotation.\n\t  You may want to double check the excel file.\n\n"));
 		}
 
-	if (any(rows.with.missing.anno)) {
-		x <- x[!rows.with.missing.anno,];
-		}
-
-	# add sample names
-	colnames(x) <- c(colnames(x)[1:3], sample.ids);
+    colnames(rcc$x) <- gsub(" ", ".", colnames(rcc$x));
+	colnames(rcc$x) <- c(colnames(rcc$x)[1:3], sample.ids);
 
 	# print summary of samples
 	cat(paste("There were", length(sample.ids), "samples imported. \nNote that spaces in sample names will be replaced by dots. \n"));
 	
-	if ( length(sample.ids) > 5) {
+	if (length(sample.ids) > 5) {
 		cat("The first and last 3 sample names found in the dataset are:\n");
-		cat(paste(c(sample.ids[1:3],rev(sample.ids)[1:3])));
+		cat(paste(c(sample.ids[1:3], rev(sample.ids)[1:3])));
 		}
 	else {
 		cat("The sample names found in the dataset are:\n");
@@ -141,8 +123,8 @@ read.xls.RCC <- function(xls, sheet = 1, perl, sample.id.row = "File.Name") {
 		}
 
 	# print summary of genes 
-	cat(paste("\n\nThere were", nrow(x), "genes imported with the following Code Class breakdown:"));
-	print(table(x$Code.Class));
+	cat(paste("\n\nThere were", nrow(rcc$x), "genes imported with the following Code Class breakdown:"));
+	print(table(rcc$x[, 'Code.Class']));
 
 	class(rcc) <- 'NanoString';
 	return(rcc);
